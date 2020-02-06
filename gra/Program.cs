5c3e5300774a638e725c34782e3adeb546649c5f
Michutt <niemc.michal@gmail.com>
2020-02-06 15:48:13 +0100

    Done

---
+++ gra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gra
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string[] kolory = { "RED", "BLUE", "GREEN", "YELLOW", "BLACK", "PURPLE", "PINK", "BROWN" };
+            string[] alfabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "W", "Z" };
+            string kolor = "xd";
+            int flaga = 0;
+            int punkty = 0;
+            int[] vels = new int[10];
+            int[] dirs = new int[10];
+            Application.Run(new Form1(kolory, alfabet, kolor, flaga, vels, dirs, punkty));
+        }
+    }
+}
